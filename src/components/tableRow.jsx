7507b8356cdbe7f05ede9c968b174d526f09561b
Habibrifat <shah.newaz.habib.rifat@gmail.com>
2023-04-18 09:10:02 +0600

    fixed scroll

--- src/components/tableRow.jsx
+++ src/components/tableRow.jsx
@@ -7,7 +7,7 @@ function Table() {
   const rowNames = ["Bust", "Waist", "Heap"];
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col overflow-x-scroll">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
